5d18a16ca317925bcc213c5ce05e81c3864e937c
tzyychianliu <leonardo.ntu@gmail.com>
2017-12-10 14:40:04 -0600

    fix bugs

--- conventional_mc.py
+++ conventional_mc.py
@@ -241,7 +241,7 @@ def clustering(positions, n_clusters):
     kmeans = cluster.KMeans(n_clusters=n_clusters).fit(temp_positions)
     return kmeans.labels_
 
-def getNewPos(Pos,nbins,steps=10000):
+def getNewPos(Pos,nbins,steps):
     Pos = list(zip(*Pos)) #make it a list
     hist_matrix, edges = np.histogramdd(Pos, bins= nbins,normed=0) #hist 3D
     maxnumber = hist_matrix.max()
@@ -253,6 +253,9 @@ def getNewPos(Pos,nbins,steps=10000):
     prob = prob/prob.sum()
     #building prob as n_i/all_n
     edges = np.array(edges)
+    x_edge = edges[0][2]-edges[0][1]
+    y_edge = edges[1][2]-edges[1][1]
+    theta_edge = edges[2][2]-edges[2][1]
 
     for i in range(steps):
         # metropolis scan with prob[x][y][theta] as acceptance rate
@@ -260,13 +263,13 @@ def getNewPos(Pos,nbins,steps=10000):
         x = np.random.randint(0,nbins)
         y = np.random.randint(0,nbins)
         theta = np.random.randint(0,nbins)
-        newPos_x = edges[0][x]
-        newPos_y = edges[1][y]
-        newPos_theta = edges[2][theta]
+        newPos_x = edges[0][x]+x_edge
+        newPos_y = edges[1][y]+y_edge
+        newPos_theta = edges[2][theta]+theta_edge
         newPos = np.array([newPos_x,newPos_y,newPos_theta])
 
         if prob[x][y][theta] > np.random.uniform():
-            print("success")
+            #print("success")
             return newPos
         if i == steps-1:
             return (Pos[0][len(Pos)],Pos[1][len(Pos)],Pos[2][len(Pos)])
@@ -297,7 +300,7 @@ def simulate2(num_steps, h_stepsize=0.01):
             configs, velocities = init_config()
 
         if item%100 == 0:
-            configs = getNewPos(configs_list,5)
+            configs = getNewPos(configs_list,10,10000)
 
     return np.array(configs_list), np.array(velocities_list)
 
